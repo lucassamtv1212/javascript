aa9088046c1524c451a46472b502aa5226543540
lucas <bucetamtv1212@gmail.com>
2024-01-15 20:34:08 -0300

    comiiit 1 1 1 1  1 1

--- index3.js
+++ index3.js
@@ -1,9 +1,16 @@
-const nome ='Lucas';   // String
-const nome2 = "Lucas";
-const nome3 = `Lucas`;
-const num1 = 10; // number
-const num2 = 10.32; 
-let nomeAluno; // Undefined = não aponta pra lugar nenhum na memoria
-const sobreNomeAluno = null; // Nulo tabem não aponta pra lugar nenhum da memoria 
-const aprovado = true; // Boolean = true, false (Logico)
-console.log(aprovado);
+const nome ='Lucas';   // String 
+const nome2 = "Lucas";  
+const nome3 = `Lucas`; 
+const num1 = 10; // number 
+const num2 = 10.32;  
+let nomeAluno; // Undefined = não aponta pra lugar nenhum na memoria 
+const sobreNomeAluno = null; // Nulo tabem não aponta pra lugar nenhum da memoria  
+const aprovado = true; // Boolean = true, false (Logico) 
+ 
+console.log(typeof nome, nome);
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+a = 3;
+console.log(a, b); // 3, 2
---
+++ index4.js
@@ -0,0 +1,20 @@
+// Aritméticos + adição / concatenação (- / *) ** poteciação % resto da divisão 
+// Incremento = ++ // Decremento = --
+const num1 = 10;
+const num2 = 5;
+const num3 = 12;
+// console.log((num1 + num2) * num3);
+
+let contador = 1;
+contador++; // 2
+contador++; // 3 
+// console.log(contador--);
+// console.log(contador);
+
+let contador1 = 20;
+contador1 **= 2;
+
+// console.log(contador1);
+const num4 = 10;
+const num5 = 'Lucas'
+console.log(num4 * num5); // NaN - not a number 
