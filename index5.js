2248a9f652f0b3e19bd9aedcfc44f8769069327b
Lucas <bucetamtv1212@gmail.com>
2024-01-19 08:39:01 -0300

    comiit old

--- index5.js
+++ index5.js
@@ -2,7 +2,9 @@ let varA = `A`; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
-varA = varB;
-varB = varC;
-varC = varA; 
+const varAtemp = varA;
+varA = varB; // B
+varB = varC; // C
+varC = varA; // A
+
 console.log(varA, varB, varC);
